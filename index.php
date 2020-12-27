550b58818f6fed8adf7f81fb7c85ddb88b98a743
Jean <jeanjac13@gmail.com>
2020-12-27 17:56:28 +0100

    first

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php include_once(“home.html”); ?>
+<!DOCTYPE html>
+<html>
+
+    <style>
+
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+        }
+
+        canvas {
+            display: block;
+        }
+
+    </style>
+
+    <body>
+        <head>
+            <link rel="icon" href="favicon.png" sizes="16x16" type="image/png">
+        </head>
+        <canvas id="canvas" width="1500" height="1000">
+        </canvas>
+
+        <script src="src/paint.js"></script>
+        <script src="src/listener.js"></script>
+        <script src="src/game.js"></script>
+
+    </body>
+</html>
