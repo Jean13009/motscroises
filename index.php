0141e1b6ffb3b19dcf5829877c05fe0813bc4e51
Jean <jeanjac13@gmail.com>
2021-01-01 15:20:04 +0100

    iosCompatibility

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <head>
             <link rel="icon" href="favicon.png" sizes="16x16" type="image/png">
         </head>
-        <input type="text" id="textinput" name="textinput" autofocus onblur="focus();" style="display:inline; position:absolute; z-index: 1; top:80px; left: 80px; height:727px; width: 487px;">
+        <input type="text" id="textinput" name="textinput" autofocus onblur="focus();" style="display:inline; position:absolute; z-index: 1; top:80px; left: 80px; height:700px; width: 400px;">
 
         <canvas id="canvas" width="1500" height="1000" style="position:absolute; z-index: 2;">
         </canvas>
